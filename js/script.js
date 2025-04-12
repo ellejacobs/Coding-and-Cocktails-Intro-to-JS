ef52daf0883121e1115edf336b686ebdf11fa991
ellejacobs <ellejacobss@gmail.com>
2025-04-12 23:39:54 +0000

    second commit

--- js/script.js
+++ js/script.js
@@ -4,20 +4,27 @@ const addDevSkill = function (skill) {
     document.getElementById("coding-section").appendChild(listItem);
 };
 
-const <verb> = function (item) {
+const dance = function (item) {
    return 'I\'d like a ' + item + ', please!';
 };
  
-const <noun> = 'cocktail';
+const jersey = 'cocktail';
 
+let numberofclicks = 0
 const clickHandler = function (text) {
-
+    numberofclicks = numberofclicks + 1;
+    console.log('numberOfClicks', numberofclicks);
+    if (numberofclicks <= 3) {
+        alert(text);
+} else {
+    alert("Drink in moderation --- no more cocktails for you!");
+}
 };
 
 // Declare brainSkills array below this line
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-   document.getElementById('cocktail-section').innerHTML = <verb>(<noun>);
+   document.getElementById('cocktail-section').innerHTML = dance(jersey);
 
 });
